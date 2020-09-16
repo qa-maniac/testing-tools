1a8c84787b3167d3a2544e1f07c686c07af2f0ff
SomnambulaDreams <maksym.ivanov@elegro.eu>
2020-09-16 23:48:51 +0300

    changed Rest class for more flexible usage

--- src/main/java/tools/rest/Rest.java
+++ src/main/java/tools/rest/Rest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 
+
 public class Rest {
 
     private String baseUrl;
@@ -20,53 +21,66 @@ public class Rest {
         RestAssured.baseURI = baseUrl;
         assert(token.length <= 1);
 
-        if(token.length == 0) {
-            if      (method.equals(RequestMethod.POST))     return RestAssured.given().header("Content-Type","application/json").post(route);
-            else if (method.equals(RequestMethod.GET))      return RestAssured.given().header("Content-Type","application/json").get(route);
-            else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
-        } else {
-            RequestSpecification requestSpecification = RestAssured.given().auth().oauth2(token[0]);
-            if      (method.equals(RequestMethod.POST))     return requestSpecification.header("Content-Type","application/json").post(route);
-            else if (method.equals(RequestMethod.GET))      return requestSpecification.header("Content-Type","application/json").get(route);
-            else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
-        }
+        RequestSpecification requestSpecification = RestAssured
+                .given()
+                .header("Content-Type","application/json");
+
+        if(token.length == 1) requestSpecification = requestSpecification.auth().oauth2(token[0]);
+
+        if      (method.equals(RequestMethod.POST))     return requestSpecification.post(route);
+        else if (method.equals(RequestMethod.GET))      return requestSpecification.get(route);
+        else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
     }
 
 
     public Response executeRequest(String route, RequestMethod method, JSONObject data, String... token) throws Exception {
         RestAssured.baseURI = baseUrl;
-        assert(token.length <= 1);
 
-        if(token.length == 0) {
-            if      (method.equals(RequestMethod.POST))     return RestAssured.given().header("Content-Type","application/json").body(data.toJSONString()).post(route);
-            else if (method.equals(RequestMethod.PUT))      return RestAssured.given().header("Content-Type","application/json").body(data.toJSONString()).put(route);
-            else if (method.equals(RequestMethod.PATCH))    return RestAssured.given().header("Content-Type","application/json").body(data.toJSONString()).patch(route);
-            else if (method.equals(RequestMethod.DELETE))   return RestAssured.given().header("Content-Type","application/json").body(data.toJSONString()).delete(route);
-            else if (method.equals(RequestMethod.GET))      return RestAssured.given().header("Content-Type","application/json").body(data.toJSONString()).get(route);
-            else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
-        } else {
-            RequestSpecification requestSpecification = RestAssured.given().auth().oauth2(token[0]);
-            if      (method.equals(RequestMethod.POST))     return requestSpecification.header("Content-Type","application/json").body(data.toJSONString()).post(route);
-            else if (method.equals(RequestMethod.PUT))      return requestSpecification.header("Content-Type","application/json").body(data.toJSONString()).put(route);
-            else if (method.equals(RequestMethod.PATCH))    return requestSpecification.header("Content-Type","application/json").body(data.toJSONString()).patch(route);
-            else if (method.equals(RequestMethod.DELETE))   return requestSpecification.header("Content-Type","application/json").body(data.toJSONString()).delete(route);
-            else if (method.equals(RequestMethod.GET))      return requestSpecification.header("Content-Type","application/json").body(data.toJSONString()).get(route);
-            else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
-        }
+        assert(token.length <= 1);
+        RequestSpecification requestSpecification = RestAssured
+                .given()
+                    .header("Content-Type","application/json");
+
+        if(token.length == 0)   requestSpecification = requestSpecification.body(data.toJSONString());
+        else                    requestSpecification = requestSpecification.auth().oauth2(token[0]).body(data.toJSONString());
+
+        if      (method.equals(RequestMethod.POST))     return requestSpecification.post(route);
+        else if (method.equals(RequestMethod.PUT))      return requestSpecification.put(route);
+        else if (method.equals(RequestMethod.PATCH))    return requestSpecification.patch(route);
+        else if (method.equals(RequestMethod.DELETE))   return requestSpecification.delete(route);
+        else if (method.equals(RequestMethod.GET))      return requestSpecification.get(route);
+        else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
     }
 
 
-    public Response executeFormDataRequest(String url, RequestMethod method, RequestSpecification specification, String... token) throws Exception {
+    public Response executeRequest(String route, RequestMethod method, RequestSpecification specification, JSONObject data, String... token) throws Exception {
+        RestAssured.baseURI = baseUrl;
+
+        assert(token.length <= 1);
+        RequestSpecification requestSpecification;
+        if(token.length == 0) requestSpecification = specification.body(data.toJSONString());
+        else                  requestSpecification = specification.auth().oauth2(token[0]).body(data.toJSONString());
+
+        if      (method.equals(RequestMethod.POST))     return requestSpecification.post(route);
+        else if (method.equals(RequestMethod.PUT))      return requestSpecification.put(route);
+        else if (method.equals(RequestMethod.PATCH))    return requestSpecification.patch(route);
+        else if (method.equals(RequestMethod.DELETE))   return requestSpecification.delete(route);
+        else if (method.equals(RequestMethod.GET))      return requestSpecification.get(route);
+        else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
+    }
+
+
+    public Response executeFormDataRequest(String route, RequestMethod method, RequestSpecification specification, String... token) throws Exception {
         RestAssured.baseURI = baseUrl;
         assert (token.length <= 1);
 
         if (token.length == 1)  specification.auth().oauth2(token[0]);
 
-        if      (method.equals(RequestMethod.POST))     return specification.post(url);
-        else if (method.equals(RequestMethod.PUT))      return specification.put(url);
-        else if (method.equals(RequestMethod.PATCH))    return specification.patch(url);
-        else if (method.equals(RequestMethod.DELETE))   return specification.delete(url);
-        else if (method.equals(RequestMethod.GET))      return specification.get(url);
+        if      (method.equals(RequestMethod.POST))     return specification.post(route);
+        else if (method.equals(RequestMethod.PUT))      return specification.put(route);
+        else if (method.equals(RequestMethod.PATCH))    return specification.patch(route);
+        else if (method.equals(RequestMethod.DELETE))   return specification.delete(route);
+        else if (method.equals(RequestMethod.GET))      return specification.get(route);
         else throw new Exception("Pay attention! You are trying to use incorrect Request Method");
     }
 
